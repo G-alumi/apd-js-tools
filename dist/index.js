e2789f21c44dccf41dd3163f5f8597fb74dc1f1c
G_alumi <trainboy1024k@gmail.com>
2026-05-04 14:20:35 +0900

    refactor: follow standard TypeScript package conventions
    - add exports field to package.json
    - move dist to .gitignore (built via prepare on install)
    - remove dist from git tracking

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-export { parseApd, parseLayer } from "./lib/parser.js";
-export { blendLayers, hslToRgb, rgbToHsl } from "./lib/blend.js";
-export { flattenApd, bgraToRgba } from "./lib/image.js";
-//# sourceMappingURL=index.js.map
--- dist/lib/image.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ApdFile } from "../types/apd.js";
-/** BGRA バッファを RGBA バッファに変換して返す */
-export declare function bgraToRgba(pixels: Buffer): Buffer;
-/**
- * 全レイヤーを下から順に合成して BGRA バッファを返す。
- * 非表示レイヤーはスキップする。
- */
-export declare function flattenApd(apd: ApdFile): Buffer;
-//# sourceMappingURL=image.d.ts.map
