da8b995fb0336dcc02b229c15935dca5cf762c76
G_alumi <trainboy1024k@gmail.com>
2026-05-04 14:47:57 +0900

    refactor: remove dist from git, built via prepare on install

--- dist/lib/parser.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ApdFile, ApdLayer } from "../types/apd.js";
-/** APD ファイルから1レイヤー分のデータを読み取る */
-export declare function parseLayer(buf: Buffer, offset: number, metadataSize?: number): {
-    layer: ApdLayer;
-    nextOffset: number;
-};
-/** APD ファイルのバイナリデータを解析して {@link ApdFile} を返す */
-export declare function parseApd(buf: Buffer): ApdFile;
-//# sourceMappingURL=parser.d.ts.map
